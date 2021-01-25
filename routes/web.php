7ce78bc932395203b5e2f28f33f2dd3557d01240
Timur <borodatimur@gmail.com>
2021-01-25 20:25:29 +0300

    Главная страница

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->middleware('DontEndSlashMiddleware')->name('home.index');
 
 Route::group(['prefix' => 'service'], function () {
     Route::get('/{service:id}', [ServiceController::class, 'index'])->middleware('DontEndSlashMiddleware')->name('service.index');
